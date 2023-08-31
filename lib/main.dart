c8a8d901ef2e83bab8e5bb86c0ba15e31bf3d6ec
punkushal <spycymagar@gmail.com>
2023-08-31 23:20:17 +0545

    Material App inside Quiz class

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_basic_app/start_screen.dart';
+import 'package:quiz_basic_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 124, 12, 198),
-                Color.fromARGB(255, 129, 16, 152),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
