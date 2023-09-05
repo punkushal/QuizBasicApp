f2c6a33be1cd288f52cd1bef9eb0d989af2a7970
punkushal <spycymagar@gmail.com>
2023-09-05 18:35:44 +0545

    Contains blue print for my questions

---
+++ lib/models/quiz_questions.dart
@@ -0,0 +1,7 @@
+//this file contains blue print for my questions
+class QuizQuestions {
+  final String questionText;
+  final List<String> answers;
+//const QuizQuestions because for this app i have fixed question and answer that's why used it
+  const QuizQuestions(this.questionText, this.answers);
+}
