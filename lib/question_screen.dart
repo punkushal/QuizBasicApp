d5d2befbdf0858e833720d5b735198b31a846875
punkushal <spycymagar@gmail.com>
2023-09-08 00:25:51 +0545

    Configuring a Column

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -10,6 +10,35 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Question Screen");
+    return SizedBox(
+      //We can also wrap Column() with Center but this is also another approach
+      width: double.infinity, //this means it'll take as much width as available
+      child: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center, //align elements to the center
+        children: [
+          const Text('Questions....'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
