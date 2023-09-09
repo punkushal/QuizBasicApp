340a46df12dc9cd89348a96a162144710982aeee
punkushal <spycymagar@gmail.com>
2023-09-09 18:53:03 +0545

    Added Alignment,Margin & Padding

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -17,27 +17,34 @@ class _QuestionScreenState extends State<QuestionScreen> {
     return SizedBox(
       //We can also wrap Column() with Center but this is also another approach
       width: double.infinity, //this means it'll take as much width as available
-      child: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.center, //align elements to the center
-        children: [
-          Text(
-            currentQuestion.questionText,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        //Wrapping Column() with Container to use margin property
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, //align elements to the center
+          crossAxisAlignment: CrossAxisAlignment
+              .stretch, //Stretch elements inside Column() as much as available width
+          children: [
+            Text(
+              currentQuestion.questionText,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center, //To center the text
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          //Here we convert values of List<String>answers to another value
-          // i.e. list of answer changes to list of button
-          //Here the function inside map takes individual answer for each button which automatically performed by dart
-          //...=> works to make list of answerbutton as individual button separated with commas
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            //Here we convert values of List<String>answers to another value
+            // i.e. list of answer changes to list of button
+            //Here the function inside map takes individual answer for each button which automatically performed by dart
+            //...=> works to make list of answerbutton as individual button separated with commas
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
