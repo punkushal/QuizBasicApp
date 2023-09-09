54e4ee7eed405c0c2ea40be0a24fcc54919645d9
punkushal <spycymagar@gmail.com>
2023-09-09 19:07:51 +0545

    Added Function To Shuffle Answers List

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -4,4 +4,11 @@ class QuizQuestions {
   final List<String> answers;
 //const QuizQuestions because for this app i have fixed question and answer that's why used it
   const QuizQuestions(this.questionText, this.answers);
+
+  List<String> getShuffledAnswer() {
+    //this copies the string list of answers and assign to shuffledList
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
