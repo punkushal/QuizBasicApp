0d5d340405f9218aa66720cfe92f07c8c90d9438
punkushal <spycymagar@gmail.com>
2023-09-14 22:18:10 +0545

    Using Maps & for loops

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_basic_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswer});
+  //This is answers list selected by user
   final List<String> chosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summaryData = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      //To create map we use curly brackets inside add()
+      summaryData.add(
+        {
+          'question-index': i,
+          'question': questions[i].questionText,
+          'correct-answer': questions[i].answers[0],
+          'user-answer': chosenAnswer[i],
+        },
+      );
+    }
+    return summaryData;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
